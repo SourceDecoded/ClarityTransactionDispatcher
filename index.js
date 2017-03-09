8d3656d2a669016620d2f7edab9efd37a14f88f9
Jared Barnes <jared-barnes@leavitt.com>
2017-03-09 16:13:00 -0700

    Changed minor things like the systems names to be display name instead of Pascal Case.

--- index.js
+++ index.js
@@ -5,25 +5,18 @@ const MongoFactory_1 = require("./library/MongoFactory");
 const DispatcherApiSystem_1 = require("./systems/DispatcherApiSystem");
 const DispatcherMonitorSystem_1 = require("./systems/DispatcherMonitorSystem");
 const mongoFactory = new MongoFactory_1.default();
-var server;
-var createDispatcher = () => {
-    var app = express();
-    server = app.listen(3005, () => console.log("Disptacher Server is running locally on port 3005..."));
-    var dispatcher = new ClarityTransactionDispatcher_1.default({
-        mongoFactory: mongoFactory,
-        databaseUrl: "mongodb://localhost:27017/ClarityTransactionDispatcher"
-    });
-    dispatcher.addServiceAsync("express", app).then(() => {
-        return dispatcher.addSystemAsync(new DispatcherApiSystem_1.default());
-    }).then(() => {
-        return dispatcher.addSystemAsync(new DispatcherMonitorSystem_1.default());
-    }).catch((error) => {
-        console.log(error);
-    });
-};
-process.on("uncaughtException", (error) => {
-    server.close();
-    createDispatcher();
+let server;
+let app = express();
+server = app.listen(3005, () => console.log("Disptacher Server is running locally on port 3005..."));
+let dispatcher = new ClarityTransactionDispatcher_1.default({
+    mongoFactory: mongoFactory,
+    databaseUrl: "mongodb://localhost:27017/ClarityTransactionDispatcher"
 });
-createDispatcher();
+dispatcher.addServiceAsync("express", app).then(() => {
+    return dispatcher.addSystemAsync(new DispatcherApiSystem_1.default());
+}).then(() => {
+    return dispatcher.addSystemAsync(new DispatcherMonitorSystem_1.default());
+}).catch((error) => {
+    console.log(error);
+});
 //# sourceMappingURL=index.js.map
