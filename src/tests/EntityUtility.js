fd31aafb933c31060557b79e943db633b3e84f5d
Jared Barnes <jared-barnes@leavitt.com>
2017-08-02 16:05:38 -0600

    Added Query, and different life-cycle methods.

--- src/tests/EntityUtility.js
+++ src/tests/EntityUtility.js
@@ -1,75 +1,75 @@
 import assert from "assert";
 import EntityUtility from "./../EntityUtility";
 
-class Entity {
-    constructor() {
-        this.components = [];
-        this.components.push(
-            {
-                type: "test"
-            },
-            {
+// class Entity {
+//     constructor() {
+//         this.components = [];
+//         this.components.push(
+//             {
+//                 type: "test"
+//             },
+//             {
 
-                type: "test",
-                prop: "secondTest"
-            },
-            {
-                type: "test-2",
-                prop: "value"
-            }
-        );
-    }
-}
+//                 type: "test",
+//                 prop: "secondTest"
+//             },
+//             {
+//                 type: "test-2",
+//                 prop: "value"
+//             }
+//         );
+//     }
+// }
 
-exports["EntityUtility.constructor"] = function () {
-    var entityUtility = new EntityUtility();
-    assert.equal(entityUtility.entity, null);
+// exports["EntityUtility.constructor"] = function () {
+//     var entityUtility = new EntityUtility();
+//     assert.equal(entityUtility.entity, null);
 
-    var entity = new Entity();
+//     var entity = new Entity();
 
-    entityUtility = new EntityUtility(entity);
+//     entityUtility = new EntityUtility(entity);
 
-    assert.equal(entityUtility.entity, entity);
-};
+//     assert.equal(entityUtility.entity, entity);
+// };
 
-exports["EntityUtility.constructor: act without setting an entity."] = function () {
-    assert.throws(() => {
-        var entityUtility = new EntityUtility();
-        entityUtility.hasComponent("test");
-    });
-};
+// exports["EntityUtility.constructor: act without setting an entity."] = function () {
+//     assert.throws(() => {
+//         var entityUtility = new EntityUtility();
+//         entityUtility.hasComponent("test");
+//     });
+// };
 
-exports["EntityUtility.getComponent"] = function () {
-    var entity = new Entity();
-    var entityUtility = new EntityUtility(entity);
+// exports["EntityUtility.getComponent"] = function () {
+//     var entity = new Entity();
+//     var entityUtility = new EntityUtility(entity);
 
-    assert.equal(entityUtility.getComponent("test"), entity.components[0]);
-};
+//     assert.equal(entityUtility.getComponent("test"), entity.components[0]);
+// };
 
-exports["EntityUtility.getComponents"] = function () {
-    var entity = new Entity();
-    var entityUtility = new EntityUtility(entity);
+// exports["EntityUtility.getComponents"] = function () {
+//     var entity = new Entity();
+//     var entityUtility = new EntityUtility(entity);
 
-    var components = entityUtility.getComponents("test");
+//     var components = entityUtility.getComponents("test");
 
-    assert.equal(components[0], entity.components[0]);
-    assert.equal(components[1], entity.components[1]);
-    assert.equal(components.length, 2);
+//     assert.equal(components[0], entity.components[0]);
+//     assert.equal(components[1], entity.components[1]);
+//     assert.equal(components.length, 2);
 
-};
+// };
 
-exports["EntityUtility.hasComponent"] = function () {
-    var entity = new Entity();
-    var entityUtility = new EntityUtility(entity);
+// exports["EntityUtility.hasComponent"] = function () {
+//     var entity = new Entity();
+//     var entityUtility = new EntityUtility(entity);
 
-    assert.equal(entityUtility.hasComponent("test"), true);
-};
+//     assert.equal(entityUtility.hasComponent("test"), true);
+// };
 
-exports["EntityUtility.hasTypeComposition"] = function () {
-    var entity = new Entity();
-    var entityUtility = new EntityUtility(entity);
+// exports["EntityUtility.hasTypeComposition"] = function () {
+//     var entity = new Entity();
+//     var entityUtility = new EntityUtility(entity);
 
-    var result = entityUtility.hasTypeComposition(["test", "test-2"]);
+//     var result = entityUtility.hasTypeComposition(["test", "test-2"]);
 
-    assert.equal(result, true);
-}; 
+//     assert.equal(result, true);
+// }; 
--- src/tests/Query.js
+++ src/tests/Query.js
@@ -1,120 +1,119 @@
 import assert from "assert";
 import { MongoDb } from "./../index";
 
-var url = "mongodb://127.0.0.1:27017/test_ecs";
-var mongoDb = null;
+// const DATABASE_NAME = "clarity_transaction_dispatcher";
+// var mongoDb = null;
 
-var types = [
-    "post",
-    "comment",
-    "authorization",
-    "group",
-    "user",
-    "governance"
-];
+// var types = [
+//     "post",
+//     "comment",
+//     "authorization",
+//     "group",
+//     "user",
+//     "governance"
+// ];
 
-var versions = [
-    "0.0.1",
-    "0.0.2",
-    "0.0.3",
-    "0.0.4",
-];
+// var versions = [
+//     "0.0.1",
+//     "0.0.2",
+//     "0.0.3",
+//     "0.0.4",
+// ];
 
-var getRandomType = () => {
-    var index = parseInt(Math.random() * types.length, 10);
-    return types[index];
-};
+// var getRandomType = () => {
+//     var index = parseInt(Math.random() * types.length, 10);
+//     return types[index];
+// };
 
-var getRandomVersion = () => {
-    var index = parseInt(Math.random() * versions.length, 10);
-    return versions[index];
-};
+// var getRandomVersion = () => {
+//     var index = parseInt(Math.random() * versions.length, 10);
+//     return versions[index];
+// };
 
-var createRandomComponent = () => {
-    var component = {
-        type: getRandomType(),
-        version: getRandomVersion()
-    };
+// var createRandomComponent = () => {
+//     var component = {
+//         type: getRandomType(),
+//         version: getRandomVersion()
+//     };
 
-    return component;
-};
+//     return component;
+// };
 
-var createRandomEntity = () => {
-    var entity = {
-        _id: objectId(),
-        components: []
-    };
+// var createRandomEntity = () => {
+//     var entity = {
+//         _id: objectId(),
+//         components: []
+//     };
 
-    var amountOfComponents = parseInt(Math.random() * 10, 10);
+//     var amountOfComponents = parseInt(Math.random() * 10, 10);
 
-    for (var x = 0; x < amountOfComponents; x++) {
-        entity.components.push(createRandomComponent());
-    }
+//     for (var x = 0; x < amountOfComponents; x++) {
+//         entity.components.push(createRandomComponent());
+//     }
 
-    return entity;
-};
+//     return entity;
+// };
 
-var createEntitiesAsync = (amount, batchSize = 10000) => {
-    var batchAmount = amount / batchSize;
+// var createEntitiesAsync = (amount, batchSize = 10000) => {
+//     var batchAmount = amount / batchSize;
 
-    var batches = [];
-    for (var x = 0; x < batchAmount; x++) {
-        batches.push(new Batch(createRandomEntity, batchSize));
-    }
+//     var batches = [];
+//     for (var x = 0; x < batchAmount; x++) {
+//         batches.push(new Batch(createRandomEntity, batchSize));
+//     }
 
-    batches.reduce((promise, batch) => {
-        return promise.then(() => {
-            return batch.createEntitiesAsync();
-        });
+//     batches.reduce((promise, batch) => {
+//         return promise.then(() => {
+//             return batch.createEntitiesAsync();
+//         });
 
-    }, Promise.resolve()).then(() => {
-        console.log("DONE");
-    });
-}
+//     }, Promise.resolve()).then(() => {
+//         console.log("DONE");
+//     });
+// }
 
-class Batch {
+// class Batch {
 
-    constructor(factory, size) {
-        this.factory = factory;
-        this.size = size;
-    }
+//     constructor(factory, size) {
+//         this.factory = factory;
+//         this.size = size;
+//     }
 
-    createEntitiesAsync() {
-        return mongodb.connect(url).then((db) => {
-            var entities = [];
-            for (var x = 0; x < this.size; x++) {
-                entities.push(this.factory());
-            }
+//     createEntitiesAsync() {
+//         return mongodb.getDatabaseAync(DATABASE_NAME).then((db) => {
+//             var entities = [];
+//             for (var x = 0; x < this.size; x++) {
+//                 entities.push(this.factory());
+//             }
 
-            return entities.map((entity) => {
-                return db.collection("entities").insert(entity);
-            });
-        });
-    }
+//             return entities.map((entity) => {
+//                 return db.collection("entities").insert(entity);
+//             });
+//         });
+//     }
 
-}
+// }
 
-exports.clean = function () {
-    return mongoDb.getDatabaseAsync().then((db) => {
-        return Promise.all([
-            db.collection("entities").remove({}),
-            db.collection("systemData").remove({})
-        ]);
-    });
-};
+// exports.clean = function () {
+//     return mongoDb.getDatabaseAsync(DATABASE_NAME).then((db) => {
+//         return Promise.all([
+//             db.collection("entities").remove({}),
+//             db.collection("systemData").remove({})
+//         ]);
+//     });
+// };
 
-exports.prepare = function () {
-    mongoDb = new MongoDb({
-        isInMemory: true
-    });
-    return mongoDb.startAsync().then(()=>{
-        return mongoDb.getDatabaseAsync();
-    }).then((db)=>{
-        db.insert()
-    });
-};
+// exports.prepare = function () {
+//     mongoDb = new MongoDb({
+//         isInMemory: true
+//     });
+//     return mongoDb.startAsync().then(()=>{
+//         return mongoDb.getDatabaseAsync(DATABASE_NAME);
+//     }).then((db)=>{
+//         db.insert()
+//     });
+// };
 
-exports.destroy = function () {
-};
+// exports.destroy = function () {
+// };
 
-exports[""] = function(){};
